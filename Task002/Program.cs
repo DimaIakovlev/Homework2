16e9612aabea594bb0fc7dddf8bf801c7832f115
Yakovlev Dima <zif18@rambler.ru>
2022-10-21 19:38:58 +0500

    Add Task002

---
+++ Task002/Program.cs
@@ -0,0 +1,11 @@
+﻿//  Напишите программу, которая выводит третью цифру заданного числа или сообщает, что третьей цифры нет.
+Console.Write ("Введите трехзначное число ");
+string number  = Console.ReadLine();
+if (number.Length >= 3) 
+{
+    Console.WriteLine(number[2]);
+}
+else 
+{
+    Console.Write ("Третьей цифры в числе нет");
+}
